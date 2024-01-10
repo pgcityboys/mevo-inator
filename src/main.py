0f03594f16b1857ca6f1c4e673495d8b6fe8de89
lukrzak <lukrzak@wp.pl>
2024-01-10 16:40:00 +0100

    moved stations endpoint to other file

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
 from flask import Flask
 from logger import get_logger
 from api.route.start_page import start_api
+from api.route.stations import stations_api
 from api.route.vehicles_info import vehicles_info_api
 
 logger = get_logger("main")
+
 
 def create_app():
     logger.debug("Create app")
     app = Flask(__name__)
     app.register_blueprint(start_api, url_prefix='/api')
     app.register_blueprint(vehicles_info_api, url_prefix='/api')
+    app.register_blueprint(stations_api, url_prefix='/api')
     return app
+
 
 if __name__ == '__main__':
     app = create_app()
